22b3bae9c8c0cc0026218fee641c55d20e309458
pwhapps <pwwhapples@gmail.com>
2023-09-17 13:16:21 +0100

    Added notes to GUI code

--- gui.py
+++ gui.py
@@ -8,17 +8,18 @@ root.title("Password Generator")
 root.geometry('250x100')
 
 
-def clicked():
+def clicked():  # runs main.py on generate button click.
     result = RandomPass().final_password()
     text = my_box.config(text=result)
     return text
 
 
-def copy():
+def copy():  # copies text to clipboard.
     root.clipboard_clear()
     root.clipboard_append(my_box.cget('text'))
 
 
+# Defines label where password is displayed.
 my_box = Label(root, text='')
 my_box.pack()
 
